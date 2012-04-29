3f334396bd8927b9b75a4b3dc9dc25c70fc78585
wangtingxu <wangtingxu@wangtingxumatoMacBook-Pro.local>
2012-04-29 19:42:21 +0000

    Initial Commit

---
+++ JSWeatherDesign/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSWeatherDesign
+//
+//  Created by wangtingxu on 12-4-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
